763e62d710a95328e583dda7d5e736b2ff61e09a
lordzero0000 <lordzero0000@gmail.com>
2015-08-23 20:54:40 -0500

    updated runner.sh

--- bin/runner.sh
+++ bin/runner.sh
@@ -13,5 +13,5 @@ if [ -d $REPO ]; then
   cd /opt/cd-stuff/$REPO
   docker build -t $REPO .
   docker run -p 80:80 -d $REPO
-  curl -H "Content-Type: application/json" -X POST -d '{"username":"GORT","icon_url":"http://i.imgur.com/XumCA5s.png","text":"The application has been deployed! \n http://192.168.1.93"}' https://your.slack.hooks
+  curl -H "Content-Type: application/json" -X POST -d '{"username":"GORT","icon_url":"http://i.imgur.com/XumCA5s.png","text":"The application has been deployed!"}' https://your.slack.hooks
 fi
